f3c4b375882e500e8fa0465924c2d8dfe31bc27c
Jason van Zyl <jvanzyl@JVZ2.local>
2012-07-11 00:30:11 -0400

    o separate the shell and polyglot into their own repos, easy to layer on now

--- tesla-polyglot/tesla-polyglot-atom/src/main/java/org/sonatype/maven/polyglot/atom/parsing/Dependencies.java
+++
@@ -1,7 +0,0 @@
-package org.sonatype.maven.polyglot.atom.parsing;
-
-/**
- * @author dhanji@gmail.com (Dhanji R. Prasanna)
- */
-public class Dependencies {
-}
--- tesla-polyglot/tesla-polyglot-atom/src/main/java/org/sonatype/maven/polyglot/atom/parsing/Element.java
+++
@@ -1,9 +0,0 @@
-package org.sonatype.maven.polyglot.atom.parsing;
-
-/**
- * Represents a parsed element of an atom build file.
- *
- * @author dhanji@gmail.com (Dhanji R. Prasanna)
- */
-abstract class Element {
-}
